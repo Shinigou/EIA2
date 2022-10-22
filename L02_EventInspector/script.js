7a244cce95a4fe6f498295d91e6d236663e49650
Shinigou <92162143+Shinigou@users.noreply.github.com>
2022-10-22 17:28:05 +0200

    Aufg2 first uploud

---
+++ L02_EventInspector/script.js
@@ -0,0 +1,16 @@
+"use strict";
+/*
+Aufgabe: Aufg 02 Eventinspector
+Name: Gerrit Sudarbo
+Matrikel: 269362
+Datum: 21.10.2022
+Quellen:
+*/
+var EventInspector;
+(function (EventInspector) {
+    window.addEventListener("load", hndLoad);
+    function hndLoad(_event) {
+        let div1;
+    }
+})(EventInspector || (EventInspector = {}));
+//# sourceMappingURL=script.js.map
