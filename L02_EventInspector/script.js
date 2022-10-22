59126b07298352b4237e0f2ba4fd246a416259f5
Shinigou <92162143+Shinigou@users.noreply.github.com>
2022-10-22 23:34:47 +0200

    Aufg2 update

--- L02_EventInspector/script.js
+++ L02_EventInspector/script.js
@@ -4,13 +4,47 @@ Aufgabe: Aufg 02 Eventinspector
 Name: Gerrit Sudarbo
 Matrikel: 269362
 Datum: 21.10.2022
-Quellen:
+Quellen: Orientiert an Jason Hühne
 */
 var EventInspector;
 (function (EventInspector) {
     window.addEventListener("load", hndLoad);
     function hndLoad(_event) {
-        let div1;
+        let div0 = document.querySelector("#div0");
+        let div1 = document.querySelector("#div1");
+        document.addEventListener("mousemove", setInfoBox);
+        document.addEventListener("click", logInfo);
+        document.body.addEventListener("click", logInfo);
+        div0.addEventListener("click", logInfo);
+        div1.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+        document.body.addEventListener("keyup", logInfo);
+        div0.addEventListener("keyup", logInfo);
+        div1.addEventListener("keyup", logInfo);
     }
+    function setInfoBox(_event) {
+        let span = document.querySelector("span");
+        let posX = `${_event.clientX}`;
+        let posY = `${_event.clientY}`;
+        let posXoffset = 10 + _event.clientX;
+        let posYoffset = 10 + _event.clientY;
+        span.innerHTML = `X: ${posX}, Y: ${posY}, ${_event.target}`;
+        span.style.top = posYoffset + "px";
+        span.style.left = posXoffset + "px";
+    }
+    function logInfo(_event) {
+        console.log("Event Type: ", _event.type);
+        console.log("Event Target: ", _event.target);
+        console.log("Current Target: ", _event.currentTarget);
+        console.log("Event Object: ", _event);
+    }
+    const button = document.querySelector('button');
+    button.addEventListener('click', function (_event) {
+        const customevent = new CustomEvent('click');
+        button.dispatchEvent(customevent);
+    });
+    document.addEventListener('click', function () {
+        console.log("Hello World");
+    });
 })(EventInspector || (EventInspector = {}));
 //# sourceMappingURL=script.js.map
--- L02_EventInspector/script.ts
+++ L02_EventInspector/script.ts
@@ -4,42 +4,52 @@ Name: Gerrit Sudarbo
 Matrikel: 269362
 Datum: 21.10.2022
 Quellen: Orientiert an Jason Hühne 
-*/ 
+*/
 
 namespace EventInspector {
-    window.addEventListener("load", hndLoad); 
+    window.addEventListener("load", hndLoad);
+    
+    function hndLoad(_event: Event): void {
+        let div0: HTMLDivElement = <HTMLDivElement>document.querySelector("#div0");
+        let div1: HTMLDivElement = <HTMLDivElement>document.querySelector("#div1");
 
-function hndLoad(_event: Event): void {
-    let div0: HTMLDivElement = <HTMLDivElement>document.querySelector("#div0");
-    let div1: HTMLDivElement = <HTMLDivElement>document.querySelector("#div1");
+        document.addEventListener("mousemove", setInfoBox);
+        document.addEventListener("click", logInfo);
+        document.body.addEventListener("click", logInfo);
+        div0.addEventListener("click", logInfo);
+        div1.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+        document.body.addEventListener("keyup", logInfo);
+        div0.addEventListener("keyup", logInfo);
+        div1.addEventListener("keyup", logInfo);
+    }
 
-    document.addEventListener("mousemove", setInfoBox);
-    document.addEventListener("click", logInfo);
-    document.body.addEventListener("click", logInfo);
-    div0.addEventListener("click", logInfo);
-    div1.addEventListener("click", logInfo);
-    document.addEventListener("keyup", logInfo);
-    document.body.addEventListener("keyup", logInfo);
-    div0.addEventListener("keyup", logInfo);
-    div1.addEventListener("keyup", logInfo);
-}
+    function setInfoBox(_event: MouseEvent): void {
+        let span: HTMLSpanElement = <HTMLSpanElement>document.querySelector("span");
+        let posX: string = `${_event.clientX}`;
+        let posY: string = `${_event.clientY}`;
+        let posXoffset: number = 10 + _event.clientX;
+        let posYoffset: number = 10 + _event.clientY;
+        span.innerHTML = `X: ${posX}, Y: ${posY}, ${_event.target}`;
+        span.style.top = posYoffset + "px";
+        span.style.left = posXoffset + "px";
+    }
 
-function setInfoBox(_event: MouseEvent): void {
-    let span: HTMLSpanElement = <HTMLSpanElement>document.querySelector("span");
-    let posX: string = `${_event.clientX}`;
-    let posY: string = `${_event.clientY}`;
-    let posXoffset: number = 10 + _event.clientX;
-    let posYoffset: number = 10 + _event.clientY;
-    span.innerHTML = `X: ${posX}, Y: ${posY}, ${_event.target}`;
-    span.style.top = posXoffset + "px";
-    span.style.top = posYoffset + "px";
-}
+    function logInfo(_event: Event): void {
+        console.log("Event Type: ", _event.type);
+        console.log("Event Target: ", _event.target);
+        console.log("Current Target: ", _event.currentTarget);
+        console.log("Event Object: ", _event);
+    }
 
-function logInfo(_event: Event): void {
-    console.log("Event Type: ", _event.type);
-    console.log("Event Target: ", _event.target);
-    console.log("Current Target: ", _event.currentTarget);
-    console.log("Event Object: ", _event);
-}
+    const button = document.querySelector('button') as HTMLButtonElement;
 
+    button.addEventListener('click', function (_event) {
+        const customevent = new CustomEvent('click');
+        button.dispatchEvent(customevent);
+    });
+
+    document.addEventListener('click', function() {
+        console.log("Hello World")
+    });
 }
